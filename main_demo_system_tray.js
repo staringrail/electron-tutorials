56bacbbfea873b7bfd5cf3a4b8b9290c9ba7ed72
railgun1v9 <tempestblaze21@gmail.com>
2019-12-19 16:25:44 -0500

    working on system tray

--- main_demo_system_tray.js
+++ main_demo_system_tray.js
@@ -1,3 +1,19 @@
 const{app, BrowserWindow} = require('electron');
 const url = require('url');
-const path = require('path')
+const path = require('path');
+
+let mainWindow
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index_demo_system_tray.html'),
+        protocol: 'file',
+        slashes: true
+    }))
+}
+
+app.on('ready', createWindow);
